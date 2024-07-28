88940091a3fc1253b55af80236c46a73527461ed
Ces4rGlez <ces4rglez17@gmail.com>
2024-07-27 21:26:05 -0600

    Descripción de los cambios realizados

---
+++ src/app/components/mis-vehiculos/mis-vehiculos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-vehiculos',
+  standalone: true,
+  imports: [],
+  templateUrl: './mis-vehiculos.component.html',
+  styleUrl: './mis-vehiculos.component.css'
+})
+export class MisVehiculosComponent {
+
+}
